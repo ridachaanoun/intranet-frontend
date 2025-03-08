1ba477f643ae6e5c362c10b60f328958b69fefe4
ridachaanoun <ridachaanoun.ff.2@gmail.com>
2025-03-08 16:15:45 +0000

    create Classrooms page with filters and static data

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import NetworkView from '../views/NetworkView.vue'
+import ClassroomsView from "../views/ClassroomsView.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/network',
       name: 'network',
       component: NetworkView
+    },
+    {
+      path: '/classrooms',
+      name: 'classrooms',
+      component: ClassroomsView
     },
 
   ]
