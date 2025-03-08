c3807e473f7fa37676376404d2810f52f82263ce
ridachaanoun <ridachaanoun.ff.2@gmail.com>
2025-03-08 13:37:46 +0000

    add main layout with Navbar, Sidebar, and Footer components

--- src/App.vue
+++ src/App.vue
@@ -1,89 +1,40 @@
+
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { onMounted, onUnmounted } from 'vue'
+import { useAppStore } from './stores/app'
+import Navbar from './components/layout/Navbar.vue'
+import Sidebar from './components/layout/Sidebar.vue'
+import Footer from './components/layout/Footer.vue'
+
+const appStore = useAppStore()
+
+// Setup event listeners
+onMounted(() => {
+  window.addEventListener('resize', appStore.handleResize)
+  
+  // Uncomment to enable live clock - for now using the fixed time as specified
+  appStore.updateDateTime()
+  const interval = setInterval(appStore.updateDateTime, 1000)
+  onUnmounted(() => clearInterval(interval))
+})
+
+onUnmounted(() => {
+  window.removeEventListener('resize', appStore.handleResize)
+})
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
-
-  <h1 class="text-3xl font-bold underline bg-red-700">
-    Hello world!
-  </h1>
+  <div class="text-text-primary min-h-screen">
+    <Navbar />
+    <Sidebar />
+    
+    <main 
+      :class="`pt-16 px-4 sm:px-6 lg:px-8 pb-8 transition-all duration-300 mt-12 ${
+        appStore.sidebarOpen ? 'ml-64' : 'ml-20'
+      }`"
+    >
+      <router-view />
+      <Footer />
+    </main>
+  </div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
