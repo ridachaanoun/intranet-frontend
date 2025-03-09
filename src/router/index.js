cb5fc1285e9bf2ed149c19bbfdf18812162ee752
ridachaanoun <ridachaanoun.ff.2@gmail.com>
2025-03-09 13:04:07 +0000

    Add YouCode Store UI with search, product listing, and purchase modal

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import NetworkView from '../views/NetworkView.vue'
 import ClassroomsView from "../views/ClassroomsView.vue"
+import MarketplaceView from "../views/MarketplaceView.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +21,11 @@ const router = createRouter({
       path: '/classrooms',
       name: 'classrooms',
       component: ClassroomsView
+    },
+    {
+      path: '/marketplace',
+      name: 'marketplace',
+      component: MarketplaceView
     },
 
   ]
