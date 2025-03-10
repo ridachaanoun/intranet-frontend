efcc79e71ae27d5b25641f5527c9f9feccd2fe87
ridachaanoun <ridachaanoun.ff.2@gmail.com>
2025-03-10 15:00:26 +0000

    Create ProfileView view: Implement tab navigation and dynamic content display with user data integration

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import HomeView from '../views/HomeView.vue'
 import NetworkView from '../views/NetworkView.vue'
 import ClassroomsView from "../views/ClassroomsView.vue"
 import MarketplaceView from "../views/MarketplaceView.vue"
+import ProfileView from '@/views/ProfileView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -26,6 +27,11 @@ const router = createRouter({
       path: '/marketplace',
       name: 'marketplace',
       component: MarketplaceView
+    },
+    {
+      path: '/profile',
+      name: 'profile',
+      component: ProfileView
     },
 
   ]
