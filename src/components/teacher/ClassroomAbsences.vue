f19f89cd58ffa28c98e8eced7a59c527c952e336
ridachaanoun <ridachaanoun.ff.2@gmail.com>
2025-04-30 14:20:38 +0100

    add loading spinner to ClassroomAbsences while fetching data

--- src/components/teacher/ClassroomAbsences.vue
+++ src/components/teacher/ClassroomAbsences.vue
@@ -63,7 +63,7 @@
           </div>
         </div>
       </div>
-      
+      <div v-if="!absenceStore.isLoading">
       <div v-if="filteredAbsences.length > 0" class="overflow-x-auto">
         <table class="min-w-full divide-y divide-background-light">
           <thead class="bg-background-element">
@@ -123,6 +123,13 @@
         >
           <i class="fas fa-plus mr-1"></i> Record new absence
         </button>
+      </div>
+    </div>
+    <div v-else class="h-[32rem] flex justify-center items-center">
+        <svg class="animate-spin h-10 w-10 text-primary-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+          <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4" />
+          <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z" />
+        </svg>
       </div>
     </div>
 
